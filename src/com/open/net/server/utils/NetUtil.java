9446b9caa1ef9ab6f16e00a542d9916c84e1a2f6
Long <longhui.yang@inveno.cn>
2017-12-19 21:48:17 +0800

    工具类

---
+++ src/com/open/net/server/utils/NetUtil.java
@@ -0,0 +1,21 @@
+package com.open.net.server.utils;
+
+public final class NetUtil {
+
+	public final static void send_data_by_tcp_bio(String host,int port,byte[] data){
+		
+	}
+	
+	public final static void send_data_by_tcp_nio(String host,int port,byte[] data){
+		
+	}
+	
+	public final static void send_data_by_udp_bio(String host,int port,byte[] data){
+		
+	}
+	
+	public final static void send_data_by_udp_nio(String host,int port,byte[] data){
+		
+	}
+
+}
