4fec88889e8d3b1e5ddc64a699b45700fab94085
Long <longhui.yang@inveno.cn>
2017-12-20 18:51:03 +0800

    UDP工具类

--- src/com/open/net/server/utils/NetUtil.java
+++ src/com/open/net/server/utils/NetUtil.java
@@ -1,5 +1,12 @@
 package com.open.net.server.utils;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public final class NetUtil {
 
 	public final static void send_data_by_tcp_bio(String host,int port,byte[] data){
@@ -11,7 +18,23 @@ public final class NetUtil {
 	}
 	
 	public final static void send_data_by_udp_bio(String host,int port,byte[] data){
-		
+		DatagramSocket socket = null;
+		try {
+			InetAddress address = InetAddress.getByName(host);
+			DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+			socket = new DatagramSocket();
+			socket.send(packet);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			if(null != socket){
+				socket.close();
+			}
+		}
 	}
 	
 	public final static void send_data_by_udp_nio(String host,int port,byte[] data){
